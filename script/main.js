200966612dbaaa470db6e182f86a12eaffd3ec1b
Kanishak Kapur <kanishak.kapoor@gmail.com>
2024-08-15 20:25:59 -0700

    Background music

--- script/main.js
+++ script/main.js
@@ -1,5 +1,16 @@
 // Import the data to customize and insert them into page
 const fetchData = () => {
+  var source = "img/song.mp3"
+  var audio = document.createElement("audio");
+  //
+  audio.autoplay = true;
+  //
+  audio.load()
+  audio.addEventListener("load", function() { 
+      audio.play(); 
+  }, true);
+  audio.src = source;
+
   fetch("customize.json")
     .then(data => data.json())
     .then(data => {
